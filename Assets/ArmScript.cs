8d9da84cda9064144e9bbd79f45e2d1229a60c83
pyured <tankage06@gmail.com>
2025-08-08 15:25:41 -0400

    some small changes to rain particles
    elyjah dont kill me

---
+++ Assets/ArmScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour
+{
+    [SerializeField] private float angleOffset;
+    private Transform bodyOrigin;
+    private enum HandType
+    {
+        EvilRight,
+        Right
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
