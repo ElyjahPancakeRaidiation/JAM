53a9b93472ea6b14d71b77c89a3aa4b4f7148e93
pyured <tankage06@gmail.com>
2025-08-15 20:01:26 -0400

    p l oop

--- Assets/Scripts/objectScripts/Puzzle scripts/Level 1/LightningScript.cs
+++ Assets/Scripts/objectScripts/Puzzle scripts/Level 1/LightningScript.cs
@@ -28,7 +28,7 @@ public class LightningScript : MonoBehaviour
     private AudioSource audioSource;
     private bool playingLightning; //is lightning currently playing
     private float betweenTime; //how long between checking for lightning
-    [SerializeField] private float timeBetweenLightning;
+    [SerializeField] private Vector2Int timeBetweenLightning;
     [SerializeField] private int chanceOfLightning; //chance of lightning per check out of 100
 
     [SerializeField] private int maxCheekedUpAmt; //how many times the lightning can "cheek up" before stopping
@@ -93,7 +93,7 @@ public class LightningScript : MonoBehaviour
                 }
                 playingLightning = false;
             }
-            yield return new WaitForSeconds(timeBetweenLightning);
+            yield return new WaitForSeconds(new System.Random().Next(timeBetweenLightning.x, timeBetweenLightning.y));
         }
         lightningActive = false;
     }
