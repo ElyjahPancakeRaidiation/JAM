c6ed6a74a3e03ad84f1cb44d3e0d56d55268420a
pyured <tankage06@gmail.com>
2025-08-25 19:21:32 -0400

    most updated stuff w mobile UI settings

--- Assets/CanvasScript.cs
+++ Assets/CanvasScript.cs
@@ -1,12 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CanvasScript : MonoBehaviour
 {
-    private GameObject pauseCanvas, blackBarCanvas, thoughtBubbleObj, TransitionCanvas;
+    [Serializable]
+    class MobileButton
+    {
+        public string tag;
+        public GameObject mainButton; //the original button in the mobile layout ui
+        public GameObject editable; //the button that shows up in the editor mode
+        public Button buttonComponent;
+        public Vector2 RelativePosition { get; set; }
+        public Vector2 DefaultPostion { get; set; }
+        public Vector2 DefaultSize { get; set; }
+        public MobileButton(GameObject mainButton, string tag, Button buttonComponent)
+        {
+            this.mainButton = mainButton;
+            this.tag = tag;
+            this.buttonComponent = buttonComponent;
+            this.DefaultPostion = mainButton.GetComponent<RectTransform>().anchoredPosition;
+        }
+        public void UpdateButton()
+        {
+            var mainButtonRect = mainButton.GetComponent<RectTransform>();
+            var editableRect = editable.GetComponent<RectTransform>();
+            mainButtonRect.anchoredPosition = editableRect.anchoredPosition;
+            mainButtonRect.sizeDelta = editableRect.sizeDelta;
+        }
+        public void ResetToDefault()
+        {
+            RelativePosition = DefaultPostion;
+            var mainButtonRect = mainButton.GetComponent<RectTransform>();
+            var editableRect = editable.GetComponent<RectTransform>();
+            mainButtonRect.anchoredPosition = DefaultPostion;
+            editableRect.anchoredPosition = DefaultPostion;
+            mainButtonRect.sizeDelta = DefaultSize;
+            editableRect.sizeDelta = DefaultSize;
 
+        }
+    }
+    private GameObject pauseCanvas, blackBarCanvas, thoughtBubbleObj, TransitionCanvas;
+    private GameObject reorganizeUI;
     private bool isThoughtBubbleFollow;
 
     private GameObject player;
@@ -14,6 +54,11 @@ public class CanvasScript : MonoBehaviour
 
     [SerializeField] public AnimationClip sceneTransitionEndClip;
     [SerializeField] public AnimationClip mainMenuTransitionClip;
+    [SerializeField] private List<MobileButton> buttons;
+    [SerializeField] private List<MobileButton> editButtons;
+    [SerializeField] private Slider buttonScale;
+    private RectTransform currentDraggedObject;
+    private bool isEditMode;
 
 
     // Start is called before the first frame update
@@ -33,6 +78,33 @@ public class CanvasScript : MonoBehaviour
         thoughtBubble.triggerThoughtBubble += SetActiveThoughtBubble;
         GameManager.current.pauseEvent += setActivePauseCanvas;
         GameManager.current.unPauseEvent += deactivateActivePauseCanvas;
+
+        foreach (MobileButton button in buttons)
+        {
+            var buttonRect = button.mainButton.GetComponent<RectTransform>();
+            button.DefaultPostion = buttonRect.anchoredPosition;
+            button.DefaultSize = buttonRect.sizeDelta;
+        }
+        if (buttonScale) //if the slider is correctly passed to the script
+        {
+            buttonScale.onValueChanged.AddListener((v) =>
+            {
+                Debug.Log("hehehe");
+                UpdateButtonScale(v);
+            });
+        }
+        else
+        {
+            Debug.Log("missing slider reference in reorganize ui");
+        }
+    }
+
+    private void UpdateButtonScale(float v)
+    {
+        foreach (MobileButton button in buttons)
+        {
+            button.editable.GetComponent<RectTransform>().sizeDelta = button.DefaultSize * v;
+        }
     }
 
     // Update is called once per frame
@@ -42,6 +114,62 @@ public class CanvasScript : MonoBehaviour
         {
             thoughtBubbleObj.transform.position = player.transform.position;
         }
+        if (isEditMode)
+        {
+            if (Input.GetMouseButtonDown(0) && !currentDraggedObject)
+            {
+                CheckDraggable();
+            }
+            if (Input.GetMouseButtonDown(0) && currentDraggedObject)
+            {
+                StartCoroutine(DragButton());
+            }
+            if (Input.GetMouseButtonUp(0) && currentDraggedObject != null)
+            {
+                currentDraggedObject = null;
+            }
+        }
+    }
+
+    private IEnumerator DragButton()
+    {
+        while (currentDraggedObject)
+        {
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            reorganizeUI.GetComponent<RectTransform>(),
+            Input.mousePosition,
+            reorganizeUI.gameObject.GetComponentInParent<Canvas>().worldCamera,
+            out pos);
+            currentDraggedObject.anchoredPosition = pos;
+            yield return null;
+        }
+    }
+
+    private void CheckDraggable()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+        if (results.Count > 0)
+        {
+            GameObject hitObject = results[0].gameObject;
+            int index = buttons.FindIndex(b => b.editable == hitObject);
+            if (index != -1)
+            {
+                Debug.Log("we found smth");
+                currentDraggedObject = hitObject.GetComponent<RectTransform>();
+            }
+            else
+            {
+                Debug.Log(hitObject);
+            }
+        }
+
     }
 
     private void setActivePauseCanvas()
@@ -58,12 +186,58 @@ public class CanvasScript : MonoBehaviour
         thoughtBubbleObj.SetActive(isActive);
         isThoughtBubbleFollow = isActive;
     }
-    
+
 
     private void OnDestroy()
     {
         thoughtBubble.triggerThoughtBubble -= SetActiveThoughtBubble;
         GameManager.current.pauseEvent -= setActivePauseCanvas;
         GameManager.current.unPauseEvent -= setActivePauseCanvas;
+    }
+    public void LoadButtonForOrganization()
+    {
+        isEditMode = true;
+        Debug.Log(reorganizeUI);
+        reorganizeUI = GameObject.FindGameObjectWithTag("Reorganize");
+        if (reorganizeUI)
+        {
+            foreach (MobileButton button in buttons)
+            {
+                if (!button.editable) button.editable = Instantiate(button.mainButton, reorganizeUI.transform);
+                button.editable.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("missing reorganize UI, go get it");
+        }
+    }
+    public void ExitReorganize()
+    {
+        isEditMode = false;
+        foreach (MobileButton button in buttons)
+        {
+            if (button.editable)
+            {
+                button.UpdateButton();
+                button.editable.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("remember to turn off the reorganize ui before starting");
+            }
+        }
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerEnter);
+    }
+    public void ResetAllButtons()
+    {
+        buttonScale.value = 1;
+        foreach (MobileButton button in buttons)
+        {
+            button.ResetToDefault();
+        }
     }
 }
