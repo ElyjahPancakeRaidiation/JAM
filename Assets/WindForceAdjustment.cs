9cca7ad1216f91499be81796d43ffa87808af99e
Mohammeda384 <mohammedabedin06@gmail.com>
2025-09-20 20:50:04 -0400

    first stage of altitude changing

---
+++ Assets/WindForceAdjustment.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WindForceAdjustment : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float multiplier;
+
+    [SerializeField] float incrementValue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
